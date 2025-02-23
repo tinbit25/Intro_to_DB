f3e2f35d50deac2f738fc6f54205a7629b227a4e
tinbit25 <tinbitelias25@gmail.com>
2025-02-23 14:46:07 +0300

    tsk5

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Insert a single row into the customers table
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
