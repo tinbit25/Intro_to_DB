db130f2ddc36d1ee33d05ee58cb1c657f6e69106
tinbit25 <tinbitelias25@gmail.com>
2025-02-23 14:40:32 +0300

    fix erro2r

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Query to get full description of the books table
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND
+    TABLE_NAME = 'books';
